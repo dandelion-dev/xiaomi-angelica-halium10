27eed733f4d115dcccf0c088033b405c04ec32bd
TheKit <nekit1000@gmail.com>
2021-09-17 00:29:12 +0300

    build: produce recovery.img if deviceinfo_has_recovery_partition is true

--- build/make-bootimage.sh
+++ build/make-bootimage.sh
@@ -63,6 +63,12 @@ elif [ -n "$deviceinfo_dtb" ]; then
     cat $DTBS > $DTB
 fi
 
+if [ -n "$deviceinfo_prebuilt_dtbo" ]; then
+    DTBO="$HERE/$deviceinfo_prebuilt_dtbo"
+elif [ -n "$deviceinfo_dtbo" ]; then
+    DTBO="$(dirname "$OUT")/dtbo.img"
+fi
+
 if [ "$deviceinfo_bootimg_header_version" -eq 2 ]; then
     mkbootimg --kernel "$KERNEL" --ramdisk "$RAMDISK" --dtb "$DTB" --base $deviceinfo_flash_offset_base --kernel_offset $deviceinfo_flash_offset_kernel --ramdisk_offset $deviceinfo_flash_offset_ramdisk --second_offset $deviceinfo_flash_offset_second --tags_offset $deviceinfo_flash_offset_tags --dtb_offset $deviceinfo_flash_offset_dtb --pagesize $deviceinfo_flash_pagesize --cmdline "$deviceinfo_kernel_cmdline" -o "$OUT" --header_version $deviceinfo_bootimg_header_version --os_version $deviceinfo_bootimg_os_version --os_patch_level $deviceinfo_bootimg_os_patch_level
 else
@@ -72,22 +78,22 @@ fi
 if [ -n "$deviceinfo_bootimg_append_vbmeta" ] && $deviceinfo_bootimg_append_vbmeta; then
     python2 "$TMPDOWN/avb/avbtool" append_vbmeta_image --image "$OUT" --partition_size "$deviceinfo_bootimg_partition_size" --vbmeta_image "$TMPDOWN/vbmeta.img"
 fi
+#
+# if [ -n "$deviceinfo_bootimg_prebuilt_dtb" ]; then
+#    DTB="$HERE/$deviceinfo_bootimg_prebuilt_dtb"
+#elif [ -n "$deviceinfo_dtb" ]; then
+#    DTB="$KERNEL_OBJ/../$deviceinfo_codename.dtb"
+#    PREFIX=$KERNEL_OBJ/arch/$ARCH/boot/dts/
+#    DTBS="$PREFIX${deviceinfo_dtb// / $PREFIX}"
+#    cat $DTBS > $DTB
+#fi
 
-if [ -n "$deviceinfo_bootimg_prebuilt_dtb" ]; then
-    DTB="$HERE/$deviceinfo_bootimg_prebuilt_dtb"
-elif [ -n "$deviceinfo_dtb" ]; then
-    DTB="$KERNEL_OBJ/../$deviceinfo_codename.dtb"
-    PREFIX=$KERNEL_OBJ/arch/$ARCH/boot/dts/
-    DTBS="$PREFIX${deviceinfo_dtb// / $PREFIX}"
-    cat $DTBS > $DTB
-fi
-
-if [ -n "$deviceinfo_prebuilt_dtbo" ]; then
-    DTBO="$HERE/$deviceinfo_prebuilt_dtbo"
-elif [ -n "$deviceinfo_dtbo" ]; then
-    DTBO="$(dirname "$OUT")/dtbo.img"
-fi
-
+#if [ -n "$deviceinfo_prebuilt_dtbo" ]; then
+#    DTBO="$HERE/$deviceinfo_prebuilt_dtbo"
+#elif [ -n "$deviceinfo_dtbo" ]; then
+#    DTBO="$(dirname "$OUT")/dtbo.img"
+#fi
+#
     if [ "$deviceinfo_bootimg_header_version" -eq 2 ]; then
         EXTRA_ARGS+=" --header_version $deviceinfo_bootimg_header_version --dtb $DTB --dtb_offset $deviceinfo_flash_offset_dtb"
     fi
